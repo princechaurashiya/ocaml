b0f9d6c1f971702ca9b30c6f06af1dd26373c96a
cprincej <princechaurashiya163@gmail.com>
2024-02-06 08:58:39 +0530

    example of tree in ocaml

--- tree.ml
+++ tree.ml
@@ -1,37 +1,4 @@
-type 'a tree =
-|Leaf
-| Node of 'a * 'a tree * 'a tree
 
-
-
-let t =
-  Node(5,
-  Node (4,
-  Node(2,Leaf,Leaf),
-  Node (1,Leaf,Leaf)
-  ),
-  Node(3,
-  Node(6,Leaf,Leaf),
-  Node(7,Leaf,Leaf)
-  )
-  )
-
-  type 'a tree = 
-  |Leaf
-  |Node of 'a node 
-
-  and 'a node ={
-    value:'a;
-     left:'a tree;
-    right:'a tree
-  }
-
-    let tr =
-      Node{
-        value = 2;
-        left = Node {value=5; left = Leaf ; right= Leaf};
-        right= Node {value =3; left =Leaf; right = Leaf}
-      }
 
       (* represents
       2
@@ -58,25 +25,58 @@ let b =
      Node(3,Leaf,Leaf),
      Node(6,Leaf,Leaf)
      )
-     );;
+     );; 
+     (* or *)
+     let t  = 
+     Node(4,
+        Node(5,
+            Node(6,
+                 Node(1,Leaf,Leaf),
+                  Node(1,Leaf,Leaf)),
+            Node(7,
+                 Node(1,Leaf,Leaf),
+                 Node(1,Leaf,Leaf))
+             ),
+        Node(6,
+           Node(8,
+             Node(1,Leaf,Leaf),
+             Node(1,Leaf,Leaf)),
+           Node(9,
+              Node(1,Leaf,Leaf),
+              Node(1,Leaf,Leaf))));;
 
-
-
-
+      (*
+          3.11.2. Representation with Records   
+      *)
      type 'a tree =
      |Leaf
      |Node of 'a node
      and 'a node ={  
       value:'a;
      right : 'a tree;
-     left :'a tree;}
+     left :'a tree};;
 
      let t =
       Node {
         value = 2;
         left = Node {value = 1; left = Leaf; right = Leaf};
         right = Node {value = 3; left = Leaf; right = Leaf}
-      };;  
+      };;
+
+      (*or*)
+         let tre = 
+                Node{
+                   value =8;
+             
+               left= Node{ 
+                value = 2;
+                left = Node { value = 2; left = Leaf; right = Leaf};
+                right = Node { value = 6; left = Leaf; right = Leaf}
+                 };
+                right = Node {
+                value =9;
+                left = Node { value = 1; left = Leaf; right = Leaf};
+                right = Node { value = 8 ; left = Leaf;right = Leaf} } };;
       
 
 
@@ -88,4 +88,10 @@ let b =
       Zero
       |Succ of nat
       let rec even = function |Zero -> true | Succ m -> odd m
-      and odd = function |Zero -> false | Succ m -> even m
+      and odd = function |Zero -> false | Succ m -> even m;;
+
+
+
+    
+    
+                
