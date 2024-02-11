96b7bd0c5dd9a586d31f71fc217fc0414dde936a
cprincej <princechaurashiya163@gmail.com>
2024-02-12 01:09:43 +0530

    Project2

---
+++ duneProject/project2/bin/main.ml
@@ -0,0 +1,6 @@
+let () = print_endline "Hello, World!"
+let () =
+Printf.printf("Enter number which you want to find factorial\n");
+let x  = read_int() in 
+Printf.printf" Final reasult is : %d" (Fact.fact x) 
+
